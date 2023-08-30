c1b7c403c21c77c8cea50669eef6c7a149cf8f03
DielanRM <dielan.r.m@gmail.com>
2023-08-29 19:05:41 -0500

    primer parte completada, ya pregunta varias veces

---
+++ ejercicios/perimetros.js
@@ -0,0 +1,17 @@
+var figura = (prompt("Cuantos lados tiene la figura?"));
+
+function perimetro() {
+    //var lados = [];
+    for (let i = 0; i < figura; i++) {
+        
+        var medida = parseFloat(prompt("cuanto mide el lado?"));
+
+        //lados = [].push(medida);
+        }
+        console.log("hay vas");
+        console.log(medida);
+        //console.log(lados);
+    //return lados;
+}
+
+perimetro();
